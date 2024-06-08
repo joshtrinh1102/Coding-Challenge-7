9cb88be4b825e7b28a982843e5ed81ec53b6a836
joshtrinh1102 <nltrinh1102@gmail.com>
2024-06-07 21:35:40 -0400

    Finalize feedback display functionality.

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    let feedback= document.querySelector('feedback')
+});
+let submit= document.querySelector("submit")
+submit.addEventListener("click", event=>{
+    event.preventDefault()})
+    //prevent default;
+
+    
+feedbackDisplay.appendchild(feedback)//feedback
+//Implement form validation and feedback display logic.
+let name= document.querySelector('name',()=>{})
+let Rating= document.querySelector('Rating',()=>{})
+let comment= document.querySelector('comment',()=>{})
+console. log( `Customer's Name: ${name}`)
+console.log (`Rating:${Rating}`)
+console.log(`Comment:${commnet}`)
+  //Finalize feedback display functionality.
